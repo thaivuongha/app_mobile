5c50204fcda399fbca7292f170a6d5ec59918ad1
vuongthai <thaivuongha@gmail.com>
2026-05-23 13:39:57 +0700

    New version

--- app.config.js
+++ app.config.js
@@ -33,7 +33,7 @@ const getAppName = () => {
     "name": getAppName(),
     "slug": "embox",
     "uniqueIdentifier": getUniqueIdentifier(),
-    "version": "1.0.2",
+    "version": "1.0.3",
     "orientation": "default",
     "icon": "./assets/images/logo-ios.png",
     "scheme": "mobile",
